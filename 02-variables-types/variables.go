1575b0fbbdf98219d234e8ab553676e54aeb1662
zhangxiao <2071201327@qq.com>
2025-12-23 00:43:22 +0800

    初始化 zx-exercises练习库

---
+++ 02-variables-types/variables.go
@@ -0,0 +1,17 @@
+package variables
+
+func GetName() string {
+	return "Gopher"
+}
+
+func GetAge() int {
+	return 10
+}
+
+func IsCool() bool {
+	return true
+}
+
+func GetZeroValues() (int, string) {
+	return 0, ""
+}
